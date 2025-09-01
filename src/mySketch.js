a9ad4cd82fc74dbed82bd405b0493ade671c3498
nbogie <neillbogie@googlemail.com>
2025-09-01 02:36:43 +0100

    add more old-style NATO alphabet for base names

--- src/mySketch.js
+++ src/mySketch.js
@@ -847,7 +847,7 @@ function snapTo(val, increment) {
 }
 
 function createLandingPads(palette) {
-    const createOnePad = ({ frac, name }) => ({
+    const createOneLandingPad = ({ frac, name }) => ({
         leftX: snapTo(frac * width, config.xStep),
         width: config.padWidth,
         colour: random(palette.arr),
@@ -856,9 +856,11 @@ function createLandingPads(palette) {
         name,
     });
 
-    const baseNames = shuffle("Bravo Charlie Dog Lima".split(" "));
+    const baseNames = shuffle(
+        "Able Baker Charlie Dog Easy Fox Lima Oscar Whiskey Sierra".split(" ")
+    );
     return zipWith(baseNames, [0.2, 0.8], (name, frac) =>
-        createOnePad({
+        createOneLandingPad({
             name,
             frac,
         })
