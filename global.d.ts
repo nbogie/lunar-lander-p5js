a2e9e2433917d8e211aded779c2cc22d25bb6e24
nbogie <neillbogie@googlemail.com>
2025-09-02 23:31:06 +0100

    docs for global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,7 @@
-// This file will add both p5 instanced and global intellisence
+// This file will add types for both p5 instanced and global modes
+// It lets vscode (or other advanced editors) know about what functions and variables are available in p5.js
+// and what types those functions they accept and return.
+// This enables intellisense - auto-completion, inline documentation, and type checking.
 //
 // iirc, I originally copied this from https://github.com/Gaweph/p5-typescript-starter
 import * as p5Global from "p5/global";
