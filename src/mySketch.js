44fc0e17d1db28249812ac543e9ec0ca59e59a38
nbogie <neillbogie@googlemail.com>
2025-09-02 17:10:39 +0100

    comments only

--- src/mySketch.js
+++ src/mySketch.js
@@ -3,17 +3,20 @@
 	Lunar Lander #WCCChallenge "Pump"
 	https://openprocessing.org/sketch/2711492
     
-    Also on github at https://github.com/nbogie/lunar-lander-p5js
+	Also on github at https://github.com/nbogie/lunar-lander-p5js
 
 	This is a quick attempt at implementing a "lunar-lander"/"thrust" style game.
-	So far, the pump theme isn't very strong in this one, but I though i'd submit for fun as I did write it for the challenge. 
-	
+	So far, the pump theme isn't very strong in this one, but I though i'd submit for fun as I did write it for the challenge. 	
     
 	Currently, there's: 
 	* fuel pumped from the tank to the ship,    
 	* pump association with the winds that blow across the moon,
     (I can't decide how much atmosphere is, here!)
     
+    Slow frame-rate?  
+    If you're getting a frame rate below 60FPS, consider disabling wind with 'w' - wind is the main burden.
+    (You can also enter zen mode with 'z')
+	
     Credits: Palette is "Tundra3" from Kjetil Golid's chromotome collection https://chromotome-quicker.netlify.app/ and https://github.com/kgolid
 	
 	See all challenge submissions: 
