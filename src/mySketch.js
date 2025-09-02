e0c70b63a65db7c3866a994766535e8f2cd5099c
nbogie <neillbogie@googlemail.com>
2025-09-02 01:54:10 +0100

    add draw crescent moon fn

--- src/mySketch.js
+++ src/mySketch.js
@@ -44,6 +44,7 @@ let config = {
     windEnabled: true,
     numWindParticles: 500,
     screenShakeEnabled: true,
+    starsEnabled: true,
     debugMessagesEnabled: true,
     rainbowWindEnabled: false,
     drawSunAsLines: false,
@@ -90,12 +91,15 @@ function draw() {
     config.screenShakeEnabled && applyAnyScreenShake();
     updateShip(world.ship);
     updateParticles();
-    drawStarfield();
+    config.starsEnabled && drawStarfield();
     if (config.drawSunAsLines) {
         drawSunWithHorizontalLines();
     } else {
         drawSunOutline();
     }
+
+    drawOtherMoon();
+
     if (config.windEnabled) {
         world.windParticles.forEach(updateWindParticle);
         drawWind();
@@ -764,6 +768,7 @@ function createWorld() {
         messages: [],
         palette,
         screenShakeAmt: 0,
+        moonShadowFraction: random(0.1, 0.5),
         bodies: [],
     };
 
@@ -905,39 +910,99 @@ function collect(n, fn) {
     }
     return arr;
 }
+function drawOtherMoon() {
+    const { x, y, radius: radiusMain, colour, shadowColour } = defaultOtherMoonData();
 
-function drawSunWithHorizontalLines() {
-    const x = width * 0.6;
-    const y = getHeightAt(x) + frameCount / 100;
+    if (!config.drawSunAsLines) {
+        push();
+        fill(world.palette.skyBackground);
+        stroke(colour);
+        strokeWeight(1);
+        circle(x, round(y), radiusMain * 2);
+        pop();
+        return;
+    }
+
     push();
     translate(x, round(y));
-    const radius = 100;
+
+    //fill in background to obscure stars, etc
     fill(world.palette.skyBackground);
     noStroke();
-    // stroke(world.palette.all[4]);
+    circle(0, 0, radiusMain * 2);
+    strokeWeight(1);
+
+    const shadowCentreX = -world.moonShadowFraction * radiusMain;
+    const shadowRadius = radiusMain;
+
+    const yStep = radiusMain / 5;
+    for (let yOff = -radiusMain + yStep / 2; yOff <= radiusMain; yOff += yStep) {
+        const chLen = lengthOfCircleChord(radiusMain, yOff);
+
+        const shadowLineLength = lengthOfCircleChord(shadowRadius, yOff);
+        const shadowEndX = constrain(shadowCentreX + 0.5 * shadowLineLength, -chLen / 2, chLen / 2);
+        const shadowStartX = -chLen / 2; //always on LHS of body
+        stroke(shadowColour);
+        line(shadowStartX, round(yOff), shadowEndX, round(yOff));
+        stroke(colour);
+        line(shadowEndX, round(yOff), chLen / 2, round(yOff));
+    }
+    pop();
+}
+
+function drawSunWithHorizontalLines() {
+    const { x, y, radius, colour } = defaultSunData();
+    push();
+
+    translate(x, round(y));
+    // to hide whatever's in background (stars, etc)
+    noStroke();
+    fill(world.palette.skyBackground);
     circle(0, 0, radius * 2);
+
+    stroke(colour);
+    strokeWeight(1);
     const yStep = radius / 8;
     for (let yOff = -radius + yStep / 2; yOff <= radius; yOff += yStep) {
-        const l = lengthOfCircleArc(radius, yOff);
-        stroke(world.palette.all[4]);
-        strokeWeight(1);
+        const l = lengthOfCircleChord(radius, yOff);
         line(-l / 2, round(yOff), l / 2, round(yOff));
     }
     pop();
 }
 
 function drawSunOutline() {
-    const x = width * 0.6;
-    const y = getHeightAt(x) + frameCount / 100;
+    const { x, y, radius, colour } = defaultSunData();
     push();
     translate(x, round(y));
-    const radius = 100;
     fill(world.palette.skyBackground);
-    stroke(world.palette.all[4]);
+    stroke(colour);
     circle(0, 0, radius * 2);
     pop();
 }
-function lengthOfCircleArc(radius, y) {
+
+function defaultOtherMoonData() {
+    const orbitR = min(width, height) / 2;
+    const t =
+        1.7 * PI +
+        -1 * map((millis() / 90000) % TWO_PI, 0, TWO_PI, 0.1 * TWO_PI, 0.9 * TWO_PI, true);
+
+    const x = width / 2 + width * 0.4 * sin(t);
+    const y = height / 2 + orbitR * cos(t);
+
+    const radius = 60;
+
+    return { x, y, radius, colour: world.palette.all[4], shadowColour: world.palette.all[3] };
+}
+
+function defaultSunData() {
+    const x = width * 0.6;
+    const y = round(getHeightAt(x) + frameCount / 100);
+    const radius = 100;
+    const colour = world.palette.all[4];
+    return { x, y, radius, colour };
+}
+
+function lengthOfCircleChord(radius, y) {
     return 2 * sqrt(radius * radius - y * y);
 }
 function keyPressed() {
@@ -966,6 +1031,10 @@ function keyPressed() {
 
     if (key === "s") {
         toggleConfigBoolean("screenShakeEnabled", "screen-shake");
+    }
+
+    if (key === "a") {
+        toggleConfigBoolean("starsEnabled", "stars");
     }
 
     if (key === "l") {
